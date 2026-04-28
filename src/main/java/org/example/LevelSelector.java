74c7f18b41b00d8b94ab8925c99a64116af9ad7c
yosef-beninson <beninsony@gmail.com>
2026-04-28 21:34:06 +0300

    started

--- src/main/java/org/example/LevelSelector.java
+++ src/main/java/org/example/LevelSelector.java
@@ -5,8 +5,6 @@ import java.awt.*;
 
 public class LevelSelector extends JPanel {
     public LevelSelector() {
-        setBounds(0, 0, Window.WIDTH, Window.HEIGHT);
-
         add(new JButton("Level 1"));
         setBackground(Color.BLUE);
     }
