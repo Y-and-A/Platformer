dc632d45278d82bd5a2401e0bf214a7bfe01a4c0
yosef-beninson <beninsony@gmail.com>
2026-05-01 15:32:34 +0300

    added tile sprites

--- src/main/java/org/example/TestGameEngine.java
+++ src/main/java/org/example/TestGameEngine.java
@@ -1,18 +1,39 @@
 package org.example;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.IOException;
 
 public class TestGameEngine {
     private TestPlayer player;
     private int[][] map;
     private int TILE_WIDTH;
     private int TILE_HEIGHT;
+    private Image rightTop;
+    private Image middleTop;
+    private Image leftTop;
+    private Image leftMiddle;
+    private Image middleMiddle;
+    private Image rightMiddle;
+    private Image leftBottom;
+    private Image middleBottom;
+    private Image rightBottom;
+    private Image floatingLeft;
+    private Image floatingMiddle;
+    private Image floatingRight;
+    private Image only1;
+    private Image only2;
+    private Image only3;
+    private Image only4;
 
 
     public TestGameEngine(TestPlayer player, int[][] map) {
         this.player = player;
         this.map = map;
+        startImages();
+
 
         TILE_WIDTH = TestWindow.WIDTH / map[0].length;
         TILE_HEIGHT = TestWindow.HEIGHT / map.length;
@@ -62,7 +83,7 @@ public class TestGameEngine {
 
         for (int r = topRow; r <= bottomRow; r++) {
             for (int c = leftCol; c <= rightCol; c++) {
-                if (map[r][c] == 1) return true;
+                if (map[r][c] != 0) return true;
             }
         }
 
@@ -73,14 +94,55 @@ public class TestGameEngine {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.GREEN);
 
+        Image tile = null;
         for (int row = 0; row < map.length; row++) {
             for (int col = 0; col < map[0].length; col++) {
-                if (map[row][col] == 1) {
-                    g2d.fillRect(col * TILE_WIDTH, row * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
-                }
+                tile = switch (map[row][col]) {
+                    case 0 -> null;
+                    case 11 -> leftTop;
+                    case 12 -> middleTop;
+                    case 13 -> rightTop;
+                    case 14 -> leftMiddle;
+                    case 15 -> middleMiddle;
+                    case 16 -> rightMiddle;
+                    case 17 -> leftBottom;
+                    case 18 -> middleBottom;
+                    case 19 -> rightBottom;
+                    case 21 -> floatingLeft;
+                    case 22 -> floatingMiddle;
+                    case 23 -> floatingRight;
+                    case 31 -> only1;
+                    case 32 -> only2;
+                    case 33 -> only3;
+                    case 34 -> only4;
+                    default -> middleTop;
+                };
+                g2d.drawImage(tile,col * TILE_WIDTH, row * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT,null);
             }
         }
-
         this.player.draw(g);
+    }
+    private void startImages(){
+        try {
+            rightTop = ImageIO.read(new File("src/main/resources/tiles/rightTop.png"));
+            middleTop = ImageIO.read(new File("src/main/resources/tiles/middleTop.png"));
+            leftTop = ImageIO.read(new File("src/main/resources/tiles/leftTop.png"));
+            leftMiddle = ImageIO.read(new File("src/main/resources/tiles/leftMiddle.png"));
+            middleMiddle = ImageIO.read(new File("src/main/resources/tiles/middleMiddle.png"));
+            rightMiddle = ImageIO.read(new File("src/main/resources/tiles/rightMiddle.png"));
+            leftBottom = ImageIO.read(new File("src/main/resources/tiles/leftBottom.png"));
+            middleBottom = ImageIO.read(new File("src/main/resources/tiles/middleBottom.png"));
+            rightBottom = ImageIO.read(new File("src/main/resources/tiles/rightBottom.png"));
+            floatingLeft = ImageIO.read(new File("src/main/resources/tiles/floatingLeft.png"));
+            floatingMiddle = ImageIO.read(new File("src/main/resources/tiles/floatingMiddle.png"));
+            floatingRight = ImageIO.read(new File("src/main/resources/tiles/floatingRight.png"));
+            only1 = ImageIO.read(new File("src/main/resources/tiles/only1.png"));
+            only2 = ImageIO.read(new File("src/main/resources/tiles/only2.png"));
+            only3 = ImageIO.read(new File("src/main/resources/tiles/only3.png"));
+            only4 = ImageIO.read(new File("src/main/resources/tiles/only4.png"));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
