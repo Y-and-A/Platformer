89d845c5c1e0fcad5ac77d4bf0aac21071051b9d
Avishai <am303212575@gmail.com>
2026-05-03 20:41:38 +0300

    improved game physics and collisions, added player wall-jump, removed unused code, refactored new code and removed the word 'test' from files names, code and comments

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -13,30 +13,37 @@ public class GamePanel extends JPanel implements Runnable {
     private final boolean[] keys = new boolean[256];
     private final boolean[] prevKeys = new boolean[256];
 
-    public int[][] maps(int level){
+    public GamePanel() {
+        player = new Player();
+
+        player.x = 1100;
+        player.y = 500;
+
+        setBackground(Color.CYAN);
+
         /*
-        guide
-         0 =air
+        guide 0 =air
         11-13 top layer
         14-16 middle layer
         17-19 bottom layer
 
         21-13 floating blocks
-        31 -34 one part grass rest dirt
+        31 -34 on part grass rest dirt
          */
-        int[][] level1 = new int[][]{
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 21, 23, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 21, 23, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 21, 22, 23, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 23, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 13, 0},
-                {0, 0, 0, 0, 0, 0, 11, 12, 13, 0, 0, 0, 0, 0, 0, 0, 14, 16, 0},
-                {0, 0, 0, 0, 0, 14, 15, 15, 16, 0, 0, 0, 0, 0, 0, 0, 14, 16, 0},
-                {0, 0, 0, 0, 0, 0, 17, 18, 19, 0, 0, 0, 0, 0, 0, 0, 14, 16, 0},
-                {0, 0, 0, 21, 23, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 16, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 19, 0},
-                {11, 13, 0, 0, 0, 0, 11, 12, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        map = new int[][] {
+                {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 21, 23, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 0 , 0 , 21, 23, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+                {0 , 21, 22, 23, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 21, 23, 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 11, 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 0 , 11, 12, 13, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 11, 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 14, 15, 15, 16, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 11, 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 0 , 17, 18, 19, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 11, 0 , 0 },
+                {0 , 0 , 0 , 21, 23, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 11, 0 , 0 },
+                {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 11, 0 , 0 },
+                {11, 13, 0 , 0 , 0 , 0 , 11, 12, 13, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
                 {14, 32, 12, 12, 12, 12, 31, 15, 32, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13},
                 {14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 16},
                 {14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 16},
@@ -44,105 +51,67 @@ public class GamePanel extends JPanel implements Runnable {
                 {14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 16}
         };
 
-        int[][] level2 = new int[][]{
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 24, 0, 0, 0, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 24, 0, 0, 0, 0, 0, 0, 11, 13, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 16, 0, 0, 0, 0, 0, 0, 40, 0},
-                {24, 0, 0, 0, 0, 0, 0, 0, 0, 14, 32, 12, 12, 12, 12, 12, 12, 61, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 15, 15, 15, 15, 15, 15, 15, 16, 0},
-                {0, 0, 24, 0, 0, 0, 0, 0, 0, 17, 18, 18, 18, 18, 18, 18, 18, 19, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 11, 12, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {12, 12, 12, 31, 15, 32, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12},
-        };
-        int[][] level3 = new int[][]{
+        gameEngine = new GameEngine(player, map);
+        setFocusable(true);
 
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {0,	0,	0,	0,	0,	0,	0,	21,	22,	23,	0,	11,	12,	12,	12,	12,	12,	13,	0},
-                {0,	0,	0,	0,	0,	0,	24,	0,	0,	0,	0,	17,	18,	18,	18,	18,	18,	19,	0},
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {0,	0,	0,	0,	0,	24,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	11,	12,	12,	12,	12,	12,	13},
-                {0,	0,	0,	0,	24,	0,	0,	0,	0,	0,	0,	0,	17,	18,	18,	18,	18,	18,	19},
-                {0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
-                {21,22,	22,	23,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0}
-
-
-        };
-
-        if (level == 1)
-            return level1;
-        else if (level == 2) {
-            return level2;
-        }
-            else if (level == 3) {
-                return level3;
-
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyPressed(java.awt.event.KeyEvent e) {
+//                System.out.println("Key pressed");
+                if (e.getKeyCode() < 256) keys[e.getKeyCode()] = true;
             }
-            return level1;
-        }
 
-    public GamePanel() {
-            player = new Player();
-
-            player.x = 0;
-            player.y = 0;
-
-            map = maps(2);
-            gameEngine = new GameEngine(player, map);
-            setFocusable(true);
-
-            addKeyListener(new java.awt.event.KeyAdapter() {
-                @Override
-                public void keyPressed(java.awt.event.KeyEvent e) {
-                    if (e.getKeyCode() < 256) keys[e.getKeyCode()] = true;
-                }
-
-                @Override
-                public void keyReleased(java.awt.event.KeyEvent e) {
-                    if (e.getKeyCode() < 256) keys[e.getKeyCode()] = false;
-                }
-            });
-        }
-
-        public void startGame () {
-            gameThread = new Thread(this);
-            gameThread.start();
-        }
-
-        public void stopGame () {
-            gameThread = null;
-        }
-
-        @Override
-        public void run () {
-
-            while (gameThread != null) {
-                gameEngine.update(keys, prevKeys);
-                System.arraycopy(keys, 0, prevKeys, 0, keys.length);
-                repaint();
-                try {
-                    Thread.sleep(16);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            @Override
+            public void keyReleased(java.awt.event.KeyEvent e) {
+//                System.out.println("Key released");
+                if (e.getKeyCode() < 256) keys[e.getKeyCode()] = false;
             }
-        }
+        });
+    }
 
-        @Override
-        protected void paintComponent (Graphics g){
-            super.paintComponent(g);
+    public void startGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 
-            gameEngine.draw(g);
+    public void stopGame() {
+        gameThread = null;
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000.0 / 60;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        long lastTime = System.nanoTime();
+
+        while (gameThread != null) {
+            long now = System.nanoTime();
+            double deltaTime = (now - lastTime) / 1000000000.0;
+            lastTime = now;
+
+            gameEngine.update(keys, prevKeys, deltaTime);
+            System.arraycopy(keys, 0, prevKeys, 0, keys.length);
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000;
+
+                if (remainingTime < 0) remainingTime = 0;
+
+                Thread.sleep((long) remainingTime);
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        gameEngine.draw(g);
+    }
+}
